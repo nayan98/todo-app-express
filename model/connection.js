369fa529197a4233d9a9faf00290cdbed40b181b
Nayan singhai <nayansinghai66@gmail.com>
2020-08-07 04:55:20 +0530

    environment variables

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = ()=>
 {
-    const URI = "mongodb+srv://nayan98:todoapp@cluster0.zd1zp.mongodb.net/todoapp"
+    const URI = "mongodb+srv://"
+    +process.env.DB_USER
+    +":"
+    +process.env.DB_PASSWORD
+    +"@cluster0.zd1zp.mongodb.net/"
+    +process.env.DB_NAME;
+    console.log(URI);
    return mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
 
