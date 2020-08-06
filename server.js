c6dc822359e96edd41ff892d35db3728ff79cf2c
Nayan singhai <nayansinghai66@gmail.com>
2020-08-06 06:18:25 +0530

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connection().catch(err=>{
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.use('/assets',express.static('assets'));
